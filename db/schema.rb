e578737b1719d4daf7a4336c1a94023a3dab7a3d
thaohuyenle <huyenyunnie@s-Thao-Huyen.local>
2017-05-23 17:08:53 +0700

    Post-pull1

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,19 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20170520063951) do
+ActiveRecord::Schema.define(version: 20170523061046) do
+
+  create_table "posts", force: :cascade do |t|
+    t.string   "title"
+    t.text     "content"
+    t.datetime "created_at",            null: false
+    t.datetime "updated_at",            null: false
+    t.integer  "user_id"
+    t.string   "post_img_file_name"
+    t.string   "post_img_content_type"
+    t.integer  "post_img_file_size"
+    t.datetime "post_img_updated_at"
+  end
 
   create_table "profiles", force: :cascade do |t|
     t.string   "name"
@@ -26,8 +38,8 @@ ActiveRecord::Schema.define(version: 20170520063951) do
   end
 
   create_table "users", force: :cascade do |t|
-    t.string   "email",                  default: "", null: false
-    t.string   "encrypted_password",     default: "", null: false
+    t.string   "email",                  default: "",    null: false
+    t.string   "encrypted_password",     default: "",    null: false
     t.string   "reset_password_token"
     t.datetime "reset_password_sent_at"
     t.datetime "remember_created_at"
@@ -35,8 +47,10 @@ ActiveRecord::Schema.define(version: 20170520063951) do
     t.datetime "confirmed_at"
     t.datetime "confirmation_sent_at"
     t.string   "unconfirmed_email"
-    t.datetime "created_at",                          null: false
-    t.datetime "updated_at",                          null: false
+    t.datetime "created_at",                             null: false
+    t.datetime "updated_at",                             null: false
+    t.boolean  "admin_role",             default: false
+    t.boolean  "user_role",              default: true
     t.index ["confirmation_token"], name: "index_users_on_confirmation_token", unique: true
     t.index ["email"], name: "index_users_on_email", unique: true
     t.index ["reset_password_token"], name: "index_users_on_reset_password_token", unique: true
